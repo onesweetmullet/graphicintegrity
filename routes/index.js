62fb6a38c6783f783c86d7c20521901a1c16055d
Peter Burke <pcburke@gmail.com>
2015-11-11 22:38:06 -0500

    stylizing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var _title = 'Graphic Integrity LLC';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Graphic Integrity LLC' });
+  res.render('index', { title: _title });
 });
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: _title });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: _title });
+})
 
 module.exports = router;
